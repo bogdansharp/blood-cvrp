512e421a6c37d29ee3c7b17d6a2e08218b7a4f71
bogdansharp <bogdansharpy@gmail.com>
2026-05-15 16:14:08 +0100

    ci: test environment added to enum fix2

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,4 +1,4 @@
-﻿from enum import Enum
+from enum import Enum
 from pathlib import Path
 
 from pydantic import Field, field_validator
@@ -9,7 +9,7 @@ class EnvironmentType(str, Enum):
     DEVELOPMENT = "development"
     PRODUCTION = "production"
     STAGING = "staging"
-    TEST = "test2"
+    TEST = "test"
 
 
 class Settings(BaseSettings):
