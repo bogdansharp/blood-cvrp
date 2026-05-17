cb5e8d4382522bd086c24438f997e395a4ede022
bogdansharp <bogdansharpy@gmail.com>
2026-05-17 12:07:57 +0100

    chore: cd.yml fixed to provide playwright with actual staging url

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,15 +2,17 @@
 
 import { defineConfig } from '@playwright/test';
 
+const baseURL = process.env.PLAYWRIGHT_BASE_URL ?? 'http://127.0.0.1:5173';
+
 export default defineConfig({
 	testDir: './tests/e2e',
 	testMatch: '**/*.spec.ts',
-	use: {
-		baseURL: 'http://127.0.0.1:5173'
-	},
-	webServer: {
-		command: 'npm run dev -- --host 127.0.0.1 --port 5173',
-		url: 'http://127.0.0.1:5173',
-		reuseExistingServer: !process.env.CI
-	}
+	use: { baseURL },
+	webServer: process.env.PLAYWRIGHT_BASE_URL
+		? undefined
+		: {
+				command: 'npm run dev -- --host 127.0.0.1 --port 5173',
+				url: 'http://127.0.0.1:5173',
+				reuseExistingServer: !process.env.CI
+			}
 });
